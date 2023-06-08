4d7a589e0d348021b7cf1472d87fea97d9ca78d6
Muhammad Ikhsan <muhammad.ikhsan@efishery.com>
2023-06-08 16:26:42 +0800

    feat(initdb.sql): add payload_maps table to map source fields to destination fields for funder actions

--- initdb.sql
+++ initdb.sql
@@ -51,4 +51,15 @@ CREATE INDEX destination_fields_funder_action_id_idx ON public.destination_field
 
 ALTER TABLE public.destination_fields ADD preprocessor varchar NULL;
 
+CREATE TABLE public.payload_maps (
+	id serial NOT NULL,
+	funder_action_id int NOT NULL,
+	destination_field_id int NOT NULL,
+	source_field_id int NULL,
+	CONSTRAINT payload_maps_pk PRIMARY KEY (id),
+	CONSTRAINT payload_maps_fk_funder_action FOREIGN KEY (funder_action_id) REFERENCES public.funder_actions(id),
+	CONSTRAINT payload_maps_fk_destination_field FOREIGN KEY (destination_field_id) REFERENCES public.destination_fields(id)
+);
+CREATE INDEX payload_maps_funder_action_id_idx ON public.payload_maps (funder_action_id);
+
 
